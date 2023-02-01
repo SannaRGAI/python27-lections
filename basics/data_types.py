4eaf9dc36be56ec790b6d1d2314dbed695305e7c
SannaRGAI <saltanat9000@gmail.com>
2023-02-01 12:29:07 +0600

    numbers

---
+++ basics/data_types.py
@@ -0,0 +1,13 @@
+# =======peremennye======
+# peremennye   imennovannye ssylki na jacheiki pamyati, gde save some data for futher use pri obrashenii k name peremennoi 
+num1 = 5
+num2 = 10
+print(num1 + num2)
+
+# vvod i vyvod dannyh 
+# input - funkcia kot pozvol prinimat dannye s terminala 
+# print - funkcia kot pozvol vyvod dannye v terminal 
+number = input ("vvedite chislo")
+print("vvedite chislo-",number)
+
+# ========pravila peremennoi naimenovania ====
